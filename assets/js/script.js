7ee80ee4198cacba8ca2cc2ac7f7029c103be57a
Lucy Norman <lucynorman@mac.com>
2024-10-15 15:47:07 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+function runGame() {
+
+}
+
+function checkanswer() {
+
+}
+
+function calculatecorrectanswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+} 
+
+function displayDivisionQuestion() {
+    
+}
